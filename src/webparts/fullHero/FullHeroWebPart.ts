b13571d688d68a02c48f12b54183c437dc0308fd
scicluna <88948700+scicluna@users.noreply.github.com>
2023-11-23 18:59:49 -0500

    pretty much works now

--- src/webparts/fullHero/FullHeroWebPart.ts
+++ src/webparts/fullHero/FullHeroWebPart.ts
@@ -1,32 +1,41 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
-import {
-  type IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
+import {type IPropertyPaneConfiguration,PropertyPaneTextField} from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import HelloWorld from './components/FullHero';
+import FullHero from './components/FullHero';
+import { PropertyFieldFilePicker, IFilePickerResult } from '@pnp/spfx-property-controls/lib/PropertyFieldFilePicker';
 
 export type FullHeroProps = {
   title: string;
+  image: IFilePickerResult;
 }
 
 export default class FullHeroWebPart extends BaseClientSideWebPart<FullHeroProps> {
 
   public render(): void {
     const element: React.ReactElement<FullHeroProps> = React.createElement(
-      HelloWorld,
+      FullHero,
       {
-        title: this.properties.title
+        title: this.properties.title,
+        image: this.properties.image
       }
     );
-
     ReactDom.render(element, this.domElement);
   }
 
   protected async onInit(){
 
+  }
+
+  protected onPropertyPaneFieldChanged(propertyPath: string, oldValue: any, newValue: any): void {
+    super.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
+    
+    if (propertyPath === 'image' && newValue) {
+      // Handle the change in the image property
+      // For example, refresh the web part to reflect the new image
+      this.render();
+    }
   }
 
 
@@ -50,7 +59,37 @@ export default class FullHeroWebPart extends BaseClientSideWebPart<FullHeroProps
               groupName: "Title",
               groupFields: [
                 PropertyPaneTextField('title', {
-                  label: "Enter Title"
+                  label: "Enter Title",
+                  value: "Awesome Title"
+                })
+              ]
+            },
+            {
+
+              groupName: "Image",
+              groupFields: [
+                PropertyFieldFilePicker('image', {
+                  context: this.context as any,
+                  filePickerResult: this.properties.image,
+                  onPropertyChange: this.onPropertyPaneFieldChanged,
+                  properties: this.properties,
+                  onSave: (e: IFilePickerResult) => {
+                    console.log('FilePicker Save Event', e);
+                    if (e.fileAbsoluteUrl) {
+                      this.properties.image = e;
+                      this.render();
+                    }
+                  },
+                  onChanged: (e: IFilePickerResult) => {
+                    console.log('FilePicker Changed Event', e);
+                    if (e.fileAbsoluteUrl) {
+                      this.properties.image = e;
+                      this.render();
+                    }
+                  },
+                  key: 'filePickerId',
+                  buttonLabel: "Select Image",
+                  label : "Choose your image"
                 })
               ]
             }
--- src/webparts/fullHero/components/FullHero.tsx
+++ src/webparts/fullHero/components/FullHero.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import '../../../../dist/assets/global.css'
 import { FullHeroProps } from '../FullHeroWebPart';
 
-export default function FullHero({title}: FullHeroProps) {
-    return (
-      <section className="w-full bg-red-300" >
-        <h1>{title}</h1>
-      </section>
-    );
+export default function FullHero({title, image}: FullHeroProps) {
+
+  const sectionStyle = image ? { 
+    backgroundImage: `url(${image.fileAbsoluteUrl})`, 
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center' 
+  } : {};
+
+  return (
+    <section className="w-full h-64 bg-red-300" style={sectionStyle}>
+      <h1>{title}</h1>
+    </section>
+  );
 }
