f000dd3449224ba68693491d5caebe593897c0d4
scicluna <88948700+scicluna@users.noreply.github.com>
2023-11-23 21:11:30 -0500

    wow tailwind struggles

--- src/webparts/fullHero/FullHeroWebPart.ts
+++ src/webparts/fullHero/FullHeroWebPart.ts
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
-import {type IPropertyPaneConfiguration,PropertyPaneTextField} from '@microsoft/sp-property-pane';
+import {type IPropertyPaneConfiguration,PropertyPaneTextField, PropertyPaneDropdown} from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import FullHero from './components/FullHero';
 import { PropertyFieldFilePicker, IFilePickerResult } from '@pnp/spfx-property-controls/lib/PropertyFieldFilePicker';
 
 export type FullHeroProps = {
   title: string;
+  titleColor: "dark" | "light";
+  titlePosition: "left" | "right";
+  titleBackground: "dark" | "light";
   image: IFilePickerResult;
+  height: "short" | "medium" | "tall";
 }
 
 export default class FullHeroWebPart extends BaseClientSideWebPart<FullHeroProps> {
@@ -18,7 +22,11 @@ export default class FullHeroWebPart extends BaseClientSideWebPart<FullHeroProps
       FullHero,
       {
         title: this.properties.title,
-        image: this.properties.image
+        titleColor: this.properties.titleColor,
+        titlePosition: this.properties.titlePosition,
+        titleBackground: this.properties.titleBackground,
+        image: this.properties.image,
+        height: this.properties.height
       }
     );
     ReactDom.render(element, this.domElement);
@@ -51,45 +59,67 @@ export default class FullHeroWebPart extends BaseClientSideWebPart<FullHeroProps
     return {
       pages: [
         {
-          header: {
-            description: "Edit"
-          },
+          header: { description: "Customize your Hero" },
           groups: [
             {
-              groupName: "Title",
+              groupName: "Content",
               groupFields: [
                 PropertyPaneTextField('title', {
-                  label: "Enter Title",
-                  value: "Awesome Title"
-                })
-              ]
-            },
-            {
-
-              groupName: "Image",
-              groupFields: [
+                  label: "Title"
+                }),
                 PropertyFieldFilePicker('image', {
+                  label: "Select an Image",
                   context: this.context as any,
                   filePickerResult: this.properties.image,
                   onPropertyChange: this.onPropertyPaneFieldChanged,
                   properties: this.properties,
-                  onSave: (e: IFilePickerResult) => {
-                    console.log('FilePicker Save Event', e);
-                    if (e.fileAbsoluteUrl) {
-                      this.properties.image = e;
-                      this.render();
-                    }
+                  onSave: (e) => {
+                    this.properties.image = e;
+                    this.render();
                   },
-                  onChanged: (e: IFilePickerResult) => {
-                    console.log('FilePicker Changed Event', e);
-                    if (e.fileAbsoluteUrl) {
-                      this.properties.image = e;
-                      this.render();
-                    }
+                  onChanged: (e) => {
+                    this.properties.image = e;
+                    this.render();
                   },
-                  key: 'filePickerId',
-                  buttonLabel: "Select Image",
-                  label : "Choose your image"
+                  key: 'filePickerId'
+                })
+              ]
+            },
+            {
+              groupName: "Appearance",
+              groupFields: [
+                PropertyPaneDropdown('titleColor', {
+                  label: "Title Color",
+                  options: [
+                    { key: 'dark', text: 'Dark' },
+                    { key: 'light', text: 'Light' }
+                  ],
+                  selectedKey: 'dark'
+                }),
+                PropertyPaneDropdown('titleBackground', {
+                  label: "Title Background",
+                  options: [
+                    { key: 'dark', text: 'Dark' },
+                    { key: 'light', text: 'Light' }
+                  ],
+                  selectedKey: 'dark'
+                }),
+                PropertyPaneDropdown('titlePosition', {
+                  label: "Position",
+                  options: [
+                    { key: 'left', text: 'Left' },
+                    { key: 'right', text: 'Right' }
+                  ],
+                  selectedKey: 'left'
+                }),
+                PropertyPaneDropdown('height', {
+                  label: "Height",
+                  options: [
+                    { key: 'short', text: 'Short' },
+                    { key: 'medium', text: 'Medium' },
+                    { key: 'tall', text: 'Tall' }
+                  ],
+                  selectedKey: 'medium'
                 })
               ]
             }
--- src/webparts/fullHero/components/FullHero.tsx
+++ src/webparts/fullHero/components/FullHero.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import '../../../../dist/assets/global.css'
 import { FullHeroProps } from '../FullHeroWebPart';
 
-export default function FullHero({title, image}: FullHeroProps) {
+export default function FullHero({title, titleBackground, titleColor, titlePosition, image, height}: FullHeroProps) {
 
   const sectionStyle = image ? { 
     backgroundImage: `url(${image.fileAbsoluteUrl})`, 
@@ -10,9 +10,14 @@ export default function FullHero({title, image}: FullHeroProps) {
     backgroundPosition: 'center' 
   } : {};
 
+  const titleBackgroundStyle = titleBackground === 'light' ? 'bg-white' : 'bg-black';
+  const titleColorStyle = titleColor === 'light' ? 'text-white' : 'text-black';
+  const titlePositionStyle = titlePosition === 'left' ? 'bottom-0 left-0' : 'bottom-0 right-0';
+  const heightStyle = height === 'short' ? 'h-64' : height === 'medium' ? 'h-80' : 'h-96';
+
   return (
-    <section className="w-full h-64 bg-red-300" style={sectionStyle}>
-      <h1>{title}</h1>
+    <section className={`w-screen -translate-x-[18.15%] bg-red-300 ${heightStyle} flex relative`} style={sectionStyle}>
+      <h1 className={`p-4 bg-opacity-10 text-5xl rounded-xl absolute w-fit ${titlePositionStyle} ${titleBackgroundStyle} ${titleColorStyle}`}>{title}</h1>
     </section>
   );
 }
